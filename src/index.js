f806cce22dd659b01598d8ab8e57ea17a4165eca
Richard Davidson <r.p.davidson@outlook.com>
2021-06-15 06:50:11 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  FilterBuilder,
+  FieldDictionary,
+  Pipeline,
+  SearchProvider,
+} from "@sajari/react-search-ui";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const categoryFilter = new FilterBuilder({
+  name: "category",
+  field: "level1",
+});
+
+
+const fields = new FieldDictionary({
+  title: "name",
+  subtitle: "brand",
+});
+
+
+const pipeline = new Pipeline(
+  {
+    account: "1594153711901724220",
+    collection: "bestbuy",
+  },
+  "query"
 );
 
+ReactDOM.render(
+<SearchProvider
+    search={{
+        pipeline,
+        fields,
+        filters: [categoryFilter]
+    }}
+    searchOnLoad
+>
+    <App />
+  </SearchProvider>,
+  document.getElementById("root")
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
